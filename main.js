da87d17ba3d8434c45b0435469ac30f1b1544a9c
PeterTV <challengepeter41@gmail.com>
2022-04-15 13:48:45 +0200

    ajout du projet de base le code

---
+++ main.js
@@ -0,0 +1,28 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+let interval;
+
+let dernierUpdate = Date.now();
+
+function run() {
+    let maintenant = Date.now();
+    let dt = (maintenant - dernierUpdate) / 1000;
+    dernierUpdate = maintenant;
+   // console.log("run !");
+    update(dt);
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    draw(ctx);
+}
+
+function init() {
+   // console.log("init");
+    ctx.imageSmoothingEnabled = false;
+    ctx.msImageSmoothingEnabled = false;
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.mozImageSmoothingEnabled = false;
+    load();
+    interval = setInterval(run, 1000 / 60 );
+}
+
+init();
